28732d881fdca232686d078ebcf8c661a556ac6a
Vinayak Vijay Mohite <vinumohite25@gmail.com>
2025-11-19 23:38:22 +0530

    Modify AWS instance settings in main.tf
    Updated AWS instance configuration with new AMI, instance type, subnet, and security group.

--- main.tf
+++ main.tf
@@ -1,11 +1,13 @@
 provider "aws" {
   region = "us-east-1"
 }
-
 resource "aws_instance" "dev" {
-    ami = "ami-0f88e80871fd81e91"
-    instance_type = "t2.micro"
+  ami="ami-0bdd88bd06d16ba03"
+  instance_type = "t3.micro"
+  subnet_id              = "subnet-0cb5d0d3178d47036"  # 👈 specify subnet
+  vpc_security_group_ids = ["sg-09a85f6ddcc7facaa"]  # 👈 use security group IDs, not names
+
     tags = {
-      Name = "prod"
+      Name = "cicdprod"
     }
 }
